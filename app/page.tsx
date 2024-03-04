67a125003e4557193027932331bd27c41d74723f
Umit Vice <umitvice@gmail.com>
2024-03-04 16:40:45 +0300

    Refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>Home page</div>
+    <div>
+      <h1 className='text-3xl'>Welcome</h1>
+      <a href='/properties'>Show Properties</a>
+    </div>
   )
 }
 
