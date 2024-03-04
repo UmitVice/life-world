e295f08b7ada21718794a0036462f57e10776ea1
Umit Vice <umitvice@gmail.com>
2024-03-04 20:13:06 +0300

    Add Link to properties page for go to home

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const PropertiesPage = () => {
     return (
-      <div>Properties Page</div>
+      <div>
+        <h1 className='text-3xl'>Properties</h1>
+        <Link href='/'>Go Home</Link>
+      </div>
     )
 }
 
