5144732197840eb276d961cce60ae2af92e58869
Umit Vice <umitvice@gmail.com>
2025-02-03 22:46:49 +0300

    Update PropertyCard and PropertyDetails to use dynamic property data

--- components/PropertyCard/index.tsx
+++ components/PropertyCard/index.tsx
@@ -42,22 +42,23 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
 
       <div className="flex justify-center gap-4 text-gray-500 mb-4">
         <p>
-          <i className="fa-solid fa-bed"></i> {property?.beds} { " " }
-          <span className="md:hidden lg:inline">Beds</span>
+          <i className="fa-solid fa-bed"></i>{property?.beds} 
+          <span className="md:hidden lg:inline"> Beds</span>
         </p>
         <p>
-          <i className="fa-solid fa-bath"></i> {property?.baths} { " " }
-          <span className="md:hidden lg:inline">Baths</span>
+          <i className="fa-solid fa-bath"></i>{property?.baths}
+          <span className="md:hidden lg:inline"> Baths</span>
         </p>
         <p>
-          <i className="fa-solid fa-ruler-combined"></i>
-          {property?.square_feet} <span className="md:hidden lg:inline">sqft</span>
+          <i className="fa-solid fa-ruler-combined">{property?.square_feet}</i>
+          {property?.square_feet}<span className="md:hidden lg:inline"> sqft</span>
         </p>
       </div>
 
       <div
         className="flex justify-center gap-4 text-green-900 text-sm mb-4"
       >
+        {property?.description}
       </div>
 
       <div className="border border-gray-100 mb-5"></div>
@@ -67,7 +68,7 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
           <i
             className="fa-solid fa-location-dot text-lg text-orange-700"
           ></i>
-          <span className="text-orange-700"> Boston MA </span>
+          <span className="text-orange-700">{property?.location?.city}, {property?.location?.state}</span>
         </div>
         <Link
           href={`/properties/${property?._id}`}
--- components/PropertyDetails/index.tsx
+++ components/PropertyDetails/index.tsx
@@ -1,6 +1,7 @@
 import { IProperty } from "@/models/Property";
 import Image from 'next/image';
 import NotFound from '@/icons/not-found.png'
+import CheckMark from '@/icons/check.png'
 
 interface PropertyDetails {
   property: IProperty;
@@ -72,78 +73,33 @@ const PropertyDetails = ({ property }: PropertyDetails) => {
         className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9"
       >
         <p>
-          <i className="fa-solid fa-bed"></i> 3
-          <span className="hidden sm:inline">Beds</span>
+          <i className="fa-solid fa-bed"></i>{property?.beds}
+          <span className="hidden sm:inline"> Beds</span>
         </p>
         <p>
-          <i className="fa-solid fa-bath"></i> 2
-          <span className="hidden sm:inline">Baths</span>
+          <i className="fa-solid fa-bath"></i>{property?.baths}
+          <span className="hidden sm:inline"> Baths</span>
         </p>
         <p>
           <i className="fa-solid fa-ruler-combined"></i>
-          1,500 <span className="hidden sm:inline">sqft</span>
+          {property?.square_feet}<span className="hidden sm:inline"> sqft</span>
         </p>
       </div>
       <p className="text-gray-500 mb-4">
-        This is a beautiful apartment located near the commons
+       {property?.description}
       </p>
     </div>
 
     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
       <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
-      <ul
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none"
-      >
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Full
-          kitchen
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Washer &
-          Dryer
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Free
-          Parking
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Hot Tub
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>24/7
-          Security
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Wheelchair Accessible
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Elevator
-          Access
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Dishwasher
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Gym/Fitness Center
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Air
-          Conditioning
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Balcony/Patio
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Smart TV
-        </li>
-        <li>
-          <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Coffee
-          Maker
-        </li>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
+        {property?.amenities?.map((amenity, index) => (
+          <li key={index} className="flex items-center space-x-2 my-1">
+            <Image src={CheckMark} alt="Check Mark" width={18} height={18} className="mr-3" />
+            {amenity}
+          </li>
+        ))}
       </ul>
     </div>
     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
